a86d2546f27b78364a0b87b338a01997f11c63e2
Craig Buchanan <craibuc@gmail.com>
2014-11-24 22:45:29 -0600

    new properties; new constructor

--- OutlookContactQL/Address.h
+++ OutlookContactQL/Address.h
@@ -8,6 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Address : NSObject
+@interface Address : NSObject {
+
+//    NSString *_city;
+//    NSString *_regionCode;
+//    NSString *_postalCode;
+//    NSString *_countryCode;
+
+}
+
+@property (nonatomic,copy) NSString *label;
+@property (nonatomic,copy) NSString *street;
+@property (nonatomic,copy) NSString *city;
+@property (nonatomic,copy) NSString *regionCode;
+@property (nonatomic,copy) NSString *postalCode;
+@property (nonatomic,copy) NSString *country;
+
+/*! Creates a new Address instances given the supplied values.
+ \param aLabel - a label (e.g. Home, Work)
+ \param aCity - a city
+ \param aStreet - a street
+ \param aRegionCode - an ISO 3166-2 country subdivision codes
+ \param aPostalCode - a postal code
+ \param aCountryCode - an ISO 3166-1 alpha-2 county code
+ \returns id
+ */
+-(id) initWithData:(NSString*) aLabel
+                  :(NSString*) aStreet
+                  :(NSString*) aCity
+                  :(NSString*) aRegionCode
+                  :(NSString*) aPostalCode
+                  :(NSString*) aCountry;
 
 @end
--- OutlookContactQL/Address.m
+++ OutlookContactQL/Address.m
@@ -10,4 +10,50 @@
 
 @implementation Address
 
+-(id) initWithData :(NSString*) aLabel :(NSString*) aStreet :(NSString*) aCity :(NSString*) aRegionCode :(NSString*) aPostalCode :(NSString*) aCountry {
+    
+    self = [super init];
+    
+    if (self) {
+
+        [self setStreet: aLabel];
+        [self setStreet: aStreet];
+        [self setCity: aCity];
+        [self setRegionCode: aRegionCode];
+        [self setPostalCode: aPostalCode];
+        [self setCountry: aCountry];
+        
+    }
+    
+    return self;
+    
+}
+
+/*! The string representation of the Address instance
+ \returns NSString
+ */
+- (NSString *) description {
+    
+    // autorelease
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    
+    if ([[self street] length] != 0)
+        [array addObject: [self street]];
+
+    if ([[self city] length] != 0)
+        [array addObject: [self city]];
+    
+    if ([[self regionCode] length] !=0)
+        [array addObject: [self regionCode]];
+
+    if ([[self postalCode] length] !=0)
+        [array addObject: [self postalCode]];
+    
+    if ([[self country] length] !=0)
+        [array addObject: [self country]];
+    
+    return [array componentsJoinedByString:@", "];
+    
+}
+
 @end
