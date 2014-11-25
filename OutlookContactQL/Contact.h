87c4b1757283180a920482b566112804d0151765
Craig Buchanan <craibuc@gmail.com>
2014-11-24 22:46:18 -0600

    new properties; factory method; use API to get metadata

--- OutlookContactQL/Contact.h
+++ OutlookContactQL/Contact.h
@@ -6,8 +6,29 @@
 //  Copyright (c) 2014 Craig Buchanan. All rights reserved.
 //
 
+#import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
+#import <Address.h>
 
 @interface Contact : NSObject
+
+@property (nonatomic,copy) NSDictionary *dictionary;
+
+@property (nonatomic,retain) NSString *firstName;
+@property (nonatomic,copy) NSString *lastName;
+@property (nonatomic,copy) NSDate *birthDate;
+
+//@property (nonatomic,retain) Address *homeAddress;
+//@property (nonatomic,retain) Address *workAddress;
+@property (nonatomic,retain) NSMutableArray *addresses;
+
+@property (nonatomic,retain) NSString *emails;
+@property (nonatomic,retain) NSString *telephones;
+
+/*! Creates a new Address instances given the supplied values.
+ \param url - the .olk14Contact file's url
+ \returns id
+ */
++ (instancetype) openFromUrl: (NSURL *)url;
 
 @end
--- OutlookContactQL/Contact.m
+++ OutlookContactQL/Contact.m
@@ -10,4 +10,100 @@
 
 @implementation Contact
 
++ (instancetype) openFromUrl: (NSURL *) url {
+    
+    //
+    // create new Contact
+    //
+    Contact *contact = [[self alloc] init];
+
+    //
+    // get associated metadata
+    //
+    NSDictionary *dictionary = [self getMetadata: url];
+    
+    //
+    // deserialize
+    //
+    [Contact deserialize: contact : dictionary];
+    
+    return contact;
+    
+}
+
+/* 
+--------------------------------------------------------------------------------
+Private Methods
+--------------------------------------------------------------------------------
+*/
+
+/*! Use API to get metadata associated with the contact's .olk14Contact file
+ \param url - .olk14Contact file's location in format file:///path/to/file/x.olk14Contact
+ \returns NSDictionary
+ */
++ (NSDictionary *) getMetadata: (NSURL *) url {
+
+    MDItemRef item;
+    CFArrayRef names;
+
+    @try {
+        
+        item = MDItemCreateWithURL(NULL, (__bridge CFURLRef)url);
+        names = MDItemCopyAttributeNames(item);
+
+        return CFBridgingRelease(MDItemCopyAttributes(item, names));
+    }
+    @catch (NSException *exception) {
+        //
+    }
+    @finally {
+        CFRelease(names);
+        CFRelease(item);
+    }
+
+}
+
++ (void) deserialize: (Contact *) contact : (NSDictionary *) dictionary {
+
+    [contact setDictionary: dictionary];
+
+    [contact setFirstName: [dictionary valueForKey: @"com_microsoft_outlook_firstName"]];
+    [contact setLastName: [dictionary valueForKey: @"com_microsoft_outlook_lastName"]];
+    [contact setBirthDate: [dictionary valueForKey: @"com_microsoft_outlook_birthday"]];
+    
+    Address *homeAddress = [[Address alloc] initWithData
+                            :@"Home"
+                            :[dictionary valueForKey: @"com_microsoft_outlook_homeStreet"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_homeCity"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_homeState"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_homeZip"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_homeCountry"]
+                            ];
+//    [contact setHomeAddress: homeAddress];
+    [[contact addresses] addObject: homeAddress];
+    
+    Address *workAddress = [[Address alloc] initWithData
+                            :@"Work"
+                            :[dictionary valueForKey: @"com_microsoft_outlook_workStreet"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_workCity"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_workState"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_workZip"]
+                            :[dictionary valueForKey: @"com_microsoft_outlook_workCountry"]
+                            ];
+//    [contact setWorkAddress: workAddress];
+    [[contact addresses] addObject: workAddress];
+    
+}
+
+
+
+/*! The string representation of the Contact instance
+ \returns NSString
+ */
+- (NSString *) description {
+    
+    return [NSString stringWithFormat:@"%@ %@", [self firstName], [self lastName]];
+    
+}
+
 @end
